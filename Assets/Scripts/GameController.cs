99f3abb68f6e26cf12879736dd7e72e937c1bcf4
Mingyu Xie <mingyuxie@ucsb.edu>
2020-10-16 11:19:06 +0800

    the game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public GameObject spaceshipPrefab;
+    private GameObject spaceship;
+    [SerializeField] private float minCollisionRadius = 2.0f;
+
+    private void Awake()
+    {
+        InitializeLevel();
+    }
+
+    private void InitializeLevel()
+    {
+        //spawn asteroids
+        spawnAsteroid();
+
+        //spawn spaceship
+        spawnSpaceship();
+    }
+
+    //spawn a new asteroid
+    private void spawnAsteroid()
+    {
+        bool valid;
+        GameObject newAsteroid;
+        do
+        {
+            newAsteroid = Instantiate(asteroidPrefab);
+            newAsteroid.gameObject.tag = "Asteroid";
+
+            valid = CheckTooCloseToAsteroid(newAsteroid);
+
+        } while (valid == false);
+    }
+
+    //spawn a spaceship
+    private void spawnSpaceship()
+    {
+        bool valid;
+        do
+        {
+            spaceship = Instantiate(spaceshipPrefab);
+            spaceship.gameObject.tag = "Spaceship";
+
+            valid = CheckTooCloseToAsteroid(spaceship);
+
+        } while (valid == false);
+        return;
+    }
+
+    private bool CheckTooCloseToAsteroid (GameObject testObject)
+    {
+        GameObject[] asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
+        foreach(GameObject asteroid in asteroids)
+        {
+            if (asteroid != testObject)
+            {
+                if(Vector3.Distance(testObject.transform.position, asteroid.transform.position) < minCollisionRadius)
+                {
+                    Destroy(testObject);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float maxX = 9f;
+    [SerializeField] private float maxY = 5f;
+    float maxSpeed = 5f;
+    float rotSpeed = 180f;
+    private Rigidbody2D rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate
+        Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        if (Input.GetKey("j"))
+        {
+            z += rotSpeed * Time.deltaTime;
+            rot = Quaternion.Euler(0, 0, z);
+            transform.rotation = rot;
+        }
+        if (Input.GetKey("l"))
+        {
+            z -= rotSpeed * Time.deltaTime;
+            rot = Quaternion.Euler(0, 0, z);
+            transform.rotation = rot;
+        }
+
+        // Thrust
+        Vector3 pos = transform.position;
+        if (Input.GetKey("k"))
+        {
+            Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+            pos += rot * velocity;
+        }
+        transform.position = pos;
+
+        // Touch edge then go to the other side
+        if (transform.position.x < -maxX)
+        {
+            transform.position = new Vector2(maxX, transform.position.y);
+        }
+        else if(transform.position.x > maxX)
+        {
+            transform.position = new Vector2(-maxX, transform.position.y);
+        }
+        if (transform.position.y < -maxY)
+        {
+            transform.position = new Vector2(transform.position.x, maxY);
+        }
+        else if (transform.position.y > maxY)
+        {
+            transform.position = new Vector2(transform.position.x, -maxY);
+        }
+    }
+    private void onCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Asteroid")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
